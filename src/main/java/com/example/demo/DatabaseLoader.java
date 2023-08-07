5ac7cbedce1fd01074cfcbb71049c2efe6d153ab
ValeriaDulanto <134956424+ValeriaDulanto@users.noreply.github.com>
2023-08-07 16:19:06 +0000

    .

--- src/main/java/com/example/demo/DatabaseLoader.java
+++ src/main/java/com/example/demo/DatabaseLoader.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 public class DatabaseLoader implements CommandLineRunner {
 
 	private final InstrumentoRepository repository;
+	private final MusicoRepository repository;
 	
 
 	@Autowired
@@ -20,6 +21,19 @@ public class DatabaseLoader implements CommandLineRunner {
 		this.repository.save(new Instrumento("Guitarra", "Viento", "de madera, con caja de resonancia, 6 cuerdas templadas"));
 		this.repository.save(new Instrumento("Ukele", "Cuerda", "pequeña, de madera"));
 		this.repository.save(new Instrumento("Melodica", "Viento", "teclado pequeño de 2 octavas"));
+
+	}
+
+	@Autowired
+	public DatabaseLoader(MusicoRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		this.repository.save(new Musico("Guitarra"));
+		this.repository.save(new Musico("Ukele"));
+		this.repository.save(new Musico("Melodica"));
 
 	}
 }
